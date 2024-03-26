1b6b5d68a068219d7ce65b4bcc3a3a246ea0407c
Haiyan-Zhang1982 <haiyan.zhang1982@gmail.com>
2024-03-26 14:59:09 -0400

    finished

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -4,6 +4,7 @@ import BackButton from '../components/BackButton';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { useSnackbar } from 'notistack';
 
 const CreateBooks = () => {
   const [title, setTitle] = useState('');
@@ -11,6 +12,7 @@ const CreateBooks = () => {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar }= useSnackbar();
   const handleSaveBook = ()=>{
     const data = {
       title,
@@ -22,11 +24,12 @@ const CreateBooks = () => {
     .post("http://localhost:5555/books", data)
     .then(()=>{
       setLoading(false)
+      enqueueSnackbar('Book Created successfully', { variant: 'success'})
       navigate('/')
     })
     .catch((error)=>{
       setLoading(false)
-      alert("Error")
+      enqueueSnackbar('Error', {variant:'error'})
       console.log(error)  
     })
   }
